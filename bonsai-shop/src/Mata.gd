b4d36005fbac613d40b770408ca34e80e341c177
manuelmaceira <manuelmaceira.m@gmail.com>
2023-01-31 15:27:19 -0300

    Volver a la otra solucion para que ande el bonsai

--- bonsai-shop/src/Mata.gd
+++ bonsai-shop/src/Mata.gd
@@ -4,21 +4,14 @@ const MIN_DISTANCE := 120
 const CENTER_OFFSET := .9
 const ANGLES := 20
 
-var geo_center: Vector2
-
 
 func _ready():
-	geo_center = global_position
-	global_position = Vector2.ZERO
 	generate_points()
 
 
 func generate_points():
 	var points := PackedVector2Array()
-	var center := (
-		geo_center
-		+ Vector2(0, MIN_DISTANCE * CENTER_OFFSET)
-	)
+	var center := Vector2(0, MIN_DISTANCE * CENTER_OFFSET)
 	for i in ANGLES:
 		var point_offset := Vector2(MIN_DISTANCE, 0).rotated(
 			i * 2 * PI / ANGLES
@@ -28,8 +21,22 @@ func generate_points():
 
 
 func cut(new_polygon: PackedVector2Array):
-	var results = Geometry2D.clip_polygons(polygon, new_polygon)
+	var results = Geometry2D.clip_polygons(get_points(), new_polygon)
 	for result in results:
-		if Geometry2D.is_point_in_polygon(geo_center, result):
-			polygon = result
+		if Geometry2D.is_point_in_polygon(global_position, result):
+			set_points(result)
 			return
+
+
+func get_points() -> PackedVector2Array:
+	var new_points := PackedVector2Array()
+	for point in polygon:
+		new_points.append(to_global(point))
+	return new_points
+
+
+func set_points(new_points: PackedVector2Array):
+	var points := PackedVector2Array()
+	for point in new_points:
+		points.append(to_local(point))
+	polygon = points

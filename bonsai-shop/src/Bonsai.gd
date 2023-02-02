f7d862e24a506dca0ee31a4b16a1eef19582f18c
manuelmaceira <manuelmaceira.m@gmail.com>
2023-02-02 17:50:09 -0300

    Mas estilo

--- bonsai-shop/src/Bonsai.gd
+++ bonsai-shop/src/Bonsai.gd
@@ -2,6 +2,7 @@
 extends Node2D
 
 @export var trunk_scene : PackedScene
+@export var shadow_scene :PackedScene
 @export var leaves_scene : PackedScene
 @export var flower_scene : PackedScene
 
@@ -22,6 +23,9 @@ const MAX_BRANCH_ANGLE := PI/2
 
 const MAX_DEPTH := 3
 const BRANCH_CHANCE := .1
+const BASE_WIDTH := 30
+
+const SHADOW_SEPARATION := 64.0
 
 var leaves_spawnpoints := PackedVector2Array()
 @onready var color := trunk_gradient.sample(randf())
@@ -81,7 +85,7 @@ func generate_roots():
 	generate_trunk(Vector2(0,0), 5, .75*PI, 20, false)
 	generate_trunk(Vector2(0,0), 5, -.75*PI, 20, false)
 
-func generate_trunk(starting_point: Vector2, parts: int, base_rotation:=0.0,  width:= 30, with_leaves := true, depth:=0):
+func generate_trunk(starting_point: Vector2, parts: int, base_rotation:=0.0,  width:=BASE_WIDTH, with_leaves := true, depth:=0):
 	var trunk := trunk_scene.instantiate()
 	add_child(trunk)
 	trunk.material.set_shader_parameter("base_color",color)
@@ -97,12 +101,26 @@ func generate_trunk(starting_point: Vector2, parts: int, base_rotation:=0.0,  wi
 		if with_leaves and (i == parts-1 or (i > parts / 3.0 && randi() % 3 == 0)):
 			leaves_spawnpoints.append(last_point)
 	trunk.points = trunk_points
+	spawn_shadows(trunk_points, width)
 	for i in trunk_points.size():
 		if i == 0: continue
 		if depth < 1 and randf() < BRANCH_CHANCE:
 			var point := trunk_points[i]
 			var angle := ((randi()%2)*2-1)*randf_range(MIN_BRANCH_ANGLE,MAX_BRANCH_ANGLE)
 			generate_trunk(point, parts-i,angle,width_at(point,trunk),with_leaves, depth +1)
+
+func spawn_shadows(trunk_points: PackedVector2Array, width:int):
+	var curve = Curve2D.new()
+	for point in trunk_points:
+		curve.add_point(point)
+	var length = curve.get_baked_length()
+	print(length)
+	for i in int(floor(length / SHADOW_SEPARATION)):
+		var shadow = shadow_scene.instantiate()
+		add_child(shadow)
+		shadow.set_variation(randi())
+		shadow.position = curve.sample_baked(i*SHADOW_SEPARATION)
+		shadow.scale = width / 30.0 * Vector2(1.0,1.0)
 
 func width_at(point:Vector2, line: Line2D):
 	var curve := Curve2D.new()

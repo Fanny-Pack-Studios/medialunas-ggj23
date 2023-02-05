4c75bcebf8011f3cc2d374c717a85bd5fbf24cf6
manuelmaceira <manuelmaceira.m@gmail.com>
2023-02-05 13:11:39 -0300

    Basic UI

--- bonsai-shop/src/ScoreLabelUI.gd
+++ bonsai-shop/src/ScoreLabelUI.gd
@@ -1,11 +1,9 @@
 extends Control
 
 @onready var scoreLabel : Label= $Score
-@onready var partialScoreLabel : Label= $Score2
 @onready var timerLabel : Label= $Timer
 
 
-func _process(delta):
-	scoreLabel.text = "Croissants: %d" %Scoreboard.get_points()
-	partialScoreLabel.text = "Last Earns: %d" %Scoreboard.lastScore
-	timerLabel.text = "Time: %d" %(get_node("/root/GameScene/ActualTimer").get_time_left())
+func _process(_delta):
+	scoreLabel.text = "%d" %Scoreboard.get_points()
+	timerLabel.text = "%d" %(get_node("/root/GameScene/ActualTimer").get_time_left())
--- bonsai-shop/src/UI_TitleScreen.gd
+++ bonsai-shop/src/UI_TitleScreen.gd
@@ -1,14 +1,10 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
 func _on_press_enter_pressed():
+	$PantallaInicial.queue_free()
 	$PressEnter.visible = false
+	$Menu.visible = true
 	$MenuOptions.visible = true
 
 
@@ -16,11 +12,9 @@ func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://src/GameScene.tscn")
 
 
-func _on_settings_pressed():
-	$MenuOptions.visible = false
-	$SettingsPanel.visible = true
+func _on_quit_game_pressed():
+	get_tree().quit()
 
 
-func _on_back_settings_pressed():
-	$SettingsPanel.visible = false
-	$MenuOptions.visible = true
+func _on_continue_pressed():
+	pass  # Replace with function body.
